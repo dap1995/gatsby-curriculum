c75d36550ff9b46251711fa513ac060e0e94ee72
Daniel André da Silva <dap1995@gmail.com>
2018-02-12 22:43:58 -0200

    Added flipcard 3d parallax effect

--- src/components/Skills/skill.js
+++ src/components/Skills/skill.js
@@ -5,16 +5,20 @@ const Skill = (props) => {
     <div className="skill">
       <div className="card">
         <div className="face front">
-          <h3>{props.name}</h3>
-          <i className={props.icon}></i>
+          <div className="inner">
+            <h3>{props.name}</h3>
+            <i className={props.icon}></i>
+          </div>
         </div>
         <div className="face back">
-          <p>{props.description}</p>
-          <ul>
-          {props.projects && props.projects.map(project =>
-            <li key={`project-${project.url}`}><a href={project.url}>{project.name}</a></li>
-          )}
-          </ul>
+          <div className="inner">
+            <p>{props.description}</p>
+            <ul>
+              {props.projects && props.projects.map(project =>
+                <li key={`project-${project.url}`}><a href={project.url}>{project.name}</a></li>
+              )}
+            </ul>
+          </div>
         </div>
       </div>
     </div>
